73148aacf5ea3e842259721e4a3dcc962f37aabd
benjhatch <benjamin.hat4@gmail.com>
2019-03-25 22:38:20 -0700

    redesigned whole program

--- client.py
+++ client.py
@@ -1,18 +1,50 @@
 import socket
+import threading
+import pickle
+import time
 
-s = socket.socket()
-host = input("Please enter the hostname of the server: ")
-port = 8080
-s.connect((host,port))
-print("Connected to chat server")
+class Client:
+    def __init__(self):
+        self.run = True
+        self.latestMsg = []
+        self.s = socket.socket()
+        self.id = input("Enter your username: ")
+        self.host = input("Please enter the hostname of the server: ")
+        self.port = 8080
 
-while 1:
-    incoming_message = s.recv(1024)
-    incoming_message = incoming_message.decode()
-    print("Server: ", incoming_message)
-    print()
-    message = input("Enter a message: ")
-    message = message.encode()
-    s.send(message)
-    print("message has been sent")
-    print()
+        self.s.connect((self.host, self.port))
+        self.s.send(pickle.dumps(self.id))
+        print("Connected to server")
+
+        self.print_lock = threading.Lock()
+        self.startThreads()
+
+        self.sendMsg()
+
+    def startThreads(self):
+        self.t = threading.Thread(target=self.recvMsg)
+        self.t.start()
+
+    def recvMsg(self):
+        while self.run:
+            msg = self.s.recv(1025)
+            msg = pickle.loads(msg)
+            self.latestMsg.append(msg)
+
+    def sendMsg(self):
+        while self.run:
+            self.displayMessages()
+            to = input("To: ")
+            msg = input("Message: ")
+            print()
+            data = (to,msg,self.id)
+            self.s.send(pickle.dumps(data))
+            time.sleep(0.5)
+
+    def displayMessages(self):
+        for msg in self.latestMsg:
+            print(msg,end="\n\n")
+        del self.latestMsg[:]
+
+
+client = Client()
--- server.py
+++ server.py
@@ -1,24 +1,79 @@
 import socket
+import threading
+from queue import Queue
+import pickle
 
-s = socket.socket()
-host = socket.gethostbyname(socket.gethostname())
-print("server will start on host", host)
-port = 8080
-s.bind((host,port))
-print("Server done binding to host and port successfully")
-print()
-print("Server is waiting for incoming connections")
-s.listen(1)
-conn, addr = s.accept()
-print(addr, "Has connected to the server and is now online...")
-print()
-while 1:
-    message = input("Enter a message: ")
-    message = message.encode()
-    conn.send(message)
-    print("message has been sent")
-    print()
-    incoming_message = conn.recv(1024)
-    incoming_message = incoming_message.decode()
-    print("Client: ", incoming_message)
-    print()
+class ChatServer:
+    def __init__(self):
+        #basic variables
+        self.run = True
+        self.s = socket.socket()
+        self.host = socket.gethostbyname(socket.gethostname())
+        self.port = 8080
+        self.connections = {}
+        self.q = Queue()
+        #bind host and port
+        try:
+            self.s.bind((self.host,self.port))
+        except:
+            print("unable to bind host and port...check if port is active")
+            self.run = False
+        print("Server done binding host and port")
+        #general instructions
+        print("Server is waiting for incoming connections...")
+        print("Server will start on host", self.host)
+        print("Copy and paste ip into client input")
+        #makes sure one thing printed at a time
+        self.print_lock = threading.Lock()
+        # connect to clients
+        self.acceptClients()
+
+    #START
+    def acceptClients(self):
+        self.resetConnections()
+        while self.run:
+            self.s.listen(0)
+            conn, addr = self.s.accept()
+            print(addr, "has connected to the server")
+            self.s.setblocking(1)
+
+            name = pickle.loads(conn.recv(1024))
+            self.connections[name] = conn
+
+            self.q.put(conn)
+            self.newThread()
+
+    def newThread(self):
+        t = threading.Thread(target=self.threader)
+        t.start()
+
+    def threader(self):
+        while self.run:
+            conn = self.q.get()
+            self.recvMessage(conn)
+
+    #SERVER IN ACTION
+    def recvMessage(self,conn):
+        while self.run:
+            msg = conn.recv(1024)
+            msg = pickle.loads(msg)
+            self.redirectMsg(msg)
+
+    def redirectMsg(self,data):
+        to = data[0]
+        msg = data[1]
+        sender = data[2]
+        if to in self.connections:
+            conn = self.connections[to]
+            newMsg = sender + ": " + msg
+            conn.send(pickle.dumps(newMsg))
+        else:
+            conn = self.connections[sender]
+            conn.send(pickle.dumps(to+" is not in the server directory"))
+
+    def resetConnections(self):
+        for conn in self.connections.values():
+            conn.close()
+        self.connections = {}
+
+server = ChatServer()
