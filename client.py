ece2be4695ceb708031ee0c13466114dd730a8de
benjhatch <benjamin.hat4@gmail.com>
2019-03-26 21:53:07 -0700

    making UI, very basic

--- client.py
+++ client.py
@@ -2,24 +2,31 @@ import socket
 import threading
 import pickle
 import time
+import pygame as pg
 
 class Client:
-    def __init__(self):
+    def __init__(self, host, user, to):
+        self.host = host
+        self.user = user
+        self.to = to
+        self.count = 0
         self.run = True
-        self.latestMsg = []
+        pg.init()
+        self.myFont = pg.font.SysFont('Comic Sans MS', 25)
+        pg.font.init()
+        self.screen = pg.display.set_mode((500, 500))
+        self.allMessages = {}
+        self.allSentMessages = {}
         self.s = socket.socket()
-        self.id = input("Enter your username: ")
-        self.host = input("Please enter the hostname of the server: ")
         self.port = 8080
 
         self.s.connect((self.host, self.port))
-        self.s.send(pickle.dumps(self.id))
+        self.s.send(pickle.dumps(self.user))
         print("Connected to server")
 
         self.print_lock = threading.Lock()
         self.startThreads()
-
-        self.sendMsg()
+        self.runClient()
 
     def startThreads(self):
         self.t = threading.Thread(target=self.recvMsg)
@@ -29,22 +36,38 @@ class Client:
         while self.run:
             msg = self.s.recv(1025)
             msg = pickle.loads(msg)
-            self.latestMsg.append(msg)
+            if msg[0] in self.allMessages:
+                self.allMessages[msg[0]].append(msg)
+            else:
+                self.allMessages[msg[0]] = [msg]
+            self.displayMsg(msg[1])
 
     def sendMsg(self):
+        msg = input("Message: ")
+        data = (self.to, msg, self.user)
+        self.s.send(pickle.dumps(data))
+        self.displayMsg(msg)
+        pg.display.update()
+
+    def displayMsg(self,msg):
+        message = self.myFont.render(msg, True, (255, 255, 255))
+        self.screen.blit(message, (10, 50 + (20 * self.count)))
+        self.count += 1
+        pg.display.update()
+
+    def runClient(self):
+        self.screen.fill((0,0,0))
+        name = self.myFont.render(self.to, True, (255, 255, 255))
+        self.screen.blit(name, (250, 5))
         while self.run:
-            self.displayMessages()
-            to = input("To: ")
-            msg = input("Message: ")
-            print()
-            data = (to,msg,self.id)
-            self.s.send(pickle.dumps(data))
+            for event in pg.event.get():
+                if event.type == pg.QUIT:
+                    self.run = False
+                elif event.type == pg.KEYDOWN:
+                    if event.key == pg.K_x:
+                        self.to = input('Enter who you want to send a message to: ')
+                    if event.key == pg.K_m:
+                        self.sendMsg()
             time.sleep(0.5)
 
-    def displayMessages(self):
-        for msg in self.latestMsg:
-            print(msg,end="\n\n")
-        del self.latestMsg[:]
-
-
-client = Client()
+client = Client(input("Server ip: "), input("Enter username: "), input("Message to: "))
--- server.py
+++ server.py
@@ -15,18 +15,18 @@ class ChatServer:
         #bind host and port
         try:
             self.s.bind((self.host,self.port))
+            print("Server done binding host and port")
+            # general instructions
+            print("Server is waiting for incoming connections...")
+            print("Server will start on host", self.host)
+            print("Copy and paste ip into client input")
+            # makes sure one thing printed at a time
+            self.print_lock = threading.Lock()
+            # connect to clients
+            self.acceptClients()
         except:
-            print("unable to bind host and port...check if port is active")
+            print("unable to bind host and port...check if port is open")
             self.run = False
-        print("Server done binding host and port")
-        #general instructions
-        print("Server is waiting for incoming connections...")
-        print("Server will start on host", self.host)
-        print("Copy and paste ip into client input")
-        #makes sure one thing printed at a time
-        self.print_lock = threading.Lock()
-        # connect to clients
-        self.acceptClients()
 
     #START
     def acceptClients(self):
@@ -65,11 +65,11 @@ class ChatServer:
         sender = data[2]
         if to in self.connections:
             conn = self.connections[to]
-            newMsg = sender + ": " + msg
+            newMsg = (sender,msg)
             conn.send(pickle.dumps(newMsg))
         else:
             conn = self.connections[sender]
-            conn.send(pickle.dumps(to+" is not in the server directory"))
+            conn.send(pickle.dumps((to,"WARNING: not in the server directory")))
 
     def resetConnections(self):
         for conn in self.connections.values():
