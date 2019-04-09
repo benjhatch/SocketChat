a8b114e5feea322d6729524d3fd4b42b1e0f0ec2
benjhatch <benjamin.hat4@gmail.com>
2019-04-08 20:04:57 -0700

    upgraded text display

--- window.py
+++ window.py
@@ -7,24 +7,31 @@ class Window:
         self.order = 0
 
         self.screen = screen
-        self.myFont = pg.font.SysFont('Comic Sans MS', 25)
-        self.contact = self.myFont.render(contact, True, (255, 0, 0))
+        self.headerFont = pg.font.SysFont('Arial', 25)
+        self.textFont = pg.font.SysFont('Arial', 18)
+        self.contact = self.headerFont.render(contact, True, (255, 0, 0))
+        self.screen.fill((255, 255, 255))
+        self.screen.blit(self.contact, (250, 8))
 
         self.updateWindow()
 
     def updateWindow(self):
         self.screen.fill((255, 255, 255))
-        self.screen.blit(self.contact, (250, 25))
+        self.screen.blit(self.contact, (250, 8))
         for i in range(self.order):
             if i in self.sentMessages:
-                self.displayMessage(self.sentMessages[i], i, (0, 0, 0))
+                self.displayMessage(self.sentMessages[i], i, True, (0, 0, 0))
             else:
-                self.displayMessage(self.myMessages[i], i, (0, 0, 255))
+                self.displayMessage(self.myMessages[i], i, False, (0, 0, 255))
         pg.display.update()
 
-    def displayMessage(self, msg, index, color):
-        msg = self.myFont.render(msg.msg, True, color)
-        self.screen.blit(msg, (10, 50 + (25 * index)))
+    def displayMessage(self, msg, index, sent, color):
+        x,y = self.textFont.size(msg.msg)
+        msg = self.textFont.render(msg.msg, True, color)
+        if sent:
+            self.screen.blit(msg, (490-x, 30+(y*index)))
+        else:
+            self.screen.blit(msg, (10, 30+(y*index)))
         pg.display.update()
 
     def handleNewMsg(self, msg, sent=False):
